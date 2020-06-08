747f624f44b0426131c52cd3911fd0ef7e893abd
Asif <klmktu.asif@gmail.com>
2020-06-09 00:14:15 +0530

    Code for individual dice change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,26 +16,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('Left Button got pressed.');
+                setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;   //1 - 6
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('Right Button got pressed.');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;   //1 - 6
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
